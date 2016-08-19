fbc52eee496cd8404e9eac5bbb0ca7af97f23b03
Macarena Victoria Gallardo Stanley <Macarena Victoria Gallardo Stanley>
2016-08-19 18:48:29 -0300

    accion boton

--- app/src/main/java/cl/telematica/multimedios/app/proyectomultimedios/MainActivity.java
+++ app/src/main/java/cl/telematica/multimedios/app/proyectomultimedios/MainActivity.java
@@ -2,12 +2,17 @@ package cl.telematica.multimedios.app.proyectomultimedios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButtom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtom=(Button)
     }
 }
